13428eff3ebaf8315c5a454f61e42794b62d1faf
satamame <satax@nifty.com>
2024-02-25 23:56:09 +0900

    Doc - autodoc による自動生成以外、Markdown にした。

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 # -- Project information -----------------------------------------------------
 
 project = 'playscript'
-copyright = '2021-2022, satamame'
+copyright = '2021-2024, satamame'
 author = 'satamame'
 
 # The full version, including alpha/beta/rc tags
@@ -29,10 +29,16 @@ release = '0.2.6'
 
 language = 'ja'
 
+source_suffix = {
+    '.md': 'markdown',
+    '.rst': 'restructuredtext',
+}
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx_rtd_theme',
@@ -78,4 +84,4 @@ smv_branch_whitelist = r'^master$'
 
 
 def setup(app):
-    app.add_stylesheet('css/custom.css')  # may also be an URL
+    app.add_css_file('css/custom.css')  # may also be an URL
